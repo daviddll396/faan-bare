3b7583a03a3e905464a61e67e1fcb9eb4c650587
daviddll396 <daviddll396@gmail.com>
2025-09-22 08:46:06 +0100

    services empty state

--- src/components/reusables/ServicesGrid/ServicesGrid.tsx
+++ src/components/reusables/ServicesGrid/ServicesGrid.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ServiceCard from "../ServiceCard/ServiceCard";
 import "./servicesgrid.css";
+import AirplaneIcon from "/icons/airplane-icon.svg";
 
 interface ServiceItem {
   id: string | number;
@@ -23,6 +24,35 @@ const ServicesGrid: React.FC<Props> = ({
   actionText = "Select",
   className = "",
 }) => {
+  if (!items || items.length === 0) {
+    return (
+      <div className={`services-grid-root ${className}`}>
+        <div className="data-table-no-data">
+          <div className="no-data-icon">
+            <img
+              src={AirplaneIcon}
+              alt="No services"
+              width={48}
+              height={48}
+              className="desktop-icon"
+            />
+            <img
+              src={AirplaneIcon}
+              alt="No services"
+              width={36}
+              height={36}
+              className="mobile-icon"
+            />
+          </div>
+          <div className="no-data-title">No Services Found</div>
+          <div className="no-data-message">
+            There are no services to display at the moment.
+          </div>
+        </div>
+      </div>
+    );
+  }
+
   return (
     <div className={`services-grid-root ${className}`}>
       {items.map((it) => (
